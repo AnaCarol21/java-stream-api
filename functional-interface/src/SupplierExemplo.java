c14cb729293586938fdd773bb34f34edc8b8c4e8
Ana Lima <carol.agl@live.com>
2024-05-29 15:40:14 -0300

    add: exemplo supplier

---
+++ functional-interface/src/SupplierExemplo.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class SupplierExemplo {
+    public static void main(String[] args) {
+        // Supplier<String> saudacao = () -> "Olá, sejam bem vindos!";
+
+        List<String> listaSaudacoes = Stream.generate(()-> "Olá, sejam bem vindos!").limit(3).toList();
+
+        listaSaudacoes.forEach(System.out::println);
+    }
+}
