fb603e8005b0a6109305bbb7b1243bf204561cd7
Ana Lima <carol.agl@live.com>
2024-05-29 16:23:27 -0300

    add: exemplo Predicate

---
+++ functional-interface/src/PredicateExemplo.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class PredicateExemplo {
+    public static void main(String[] args) {
+        List<String> nomes = Arrays.asList("Juvenal", "Roberta", "Ingrid","Ana", "Yan");
+        
+        nomes.stream().filter(s -> s.length() < 5).forEach(System.out::println);
+    }
+}
