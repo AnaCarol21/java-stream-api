fbdbcaba664f73c37364ae70331fb0bc777ecbbc
AnaCarol21 <carol.agl@live.com>
2024-06-04 16:56:45 -0300

    add: desafios 2 3 4

--- functional-interface/src/desafios/DesafioTres.java
+++ functional-interface/src/desafios/DesafioTres.java
@@ -3,10 +3,10 @@ package desafios;
 import java.util.Arrays;
 import java.util.List;
 
-public class DesafioDois {
+public class DesafioTres {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);       
-        int numerosPares = numeros.stream().filter(n -> n%2==0).mapToInt(Integer::intValue).sum();
-        System.out.println(numerosPares);
+        List<Integer> numerosPositivos = numeros.stream().filter(n -> n > 0).toList();
+        System.out.println("Lista de numeros positivos: "+numerosPositivos);
     }
 }
